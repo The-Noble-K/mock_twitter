16549de62f10055f8c2c9ec4103b464ff22b0ddb
The-Noble-K <jarod.sneed@gmail.com>
2019-08-10 14:34:43 -0400

    temporary error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log in user, redirect to user show page.
     else
-      # Create an error message
+      flash[:danger] = 'Invalid email and password combination'
       render 'new'
     end
   end
