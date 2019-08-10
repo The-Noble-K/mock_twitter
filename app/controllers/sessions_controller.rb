48428da59db7ae8103551b9d25f2ddb5a858bdc8
The-Noble-K <jarod.sneed@gmail.com>
2019-08-10 14:31:16 -0400

    update create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in user, redirect to user show page.
+    else
+      # Create an error message
+      render 'new'
+    end
   end
 
   def destroy
