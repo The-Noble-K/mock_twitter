6ef4c48a976bc7dfd6960ede358afd6d95931eb6
The-Noble-K <jarod.sneed@gmail.com>
2019-08-21 11:16:48 -0400

    adjusted admin index test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
+    @user = users(:tyrion)
     @admin = users(:jarod)
     @non_admin = users(:tyrion)
   end
 
   test "index including pagination" do
-    log_in_as(@non_admin)
+    log_in_as(@user)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
@@ -17,22 +18,26 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-  #test "index as admin including pagination and delete links" do
-  #  log_in_as(@admin)
-  #  get users_path
-  #  assert_template 'users/index'
-  #  assert_select 'div.pagination'
-  #  first_page_of_users = User.paginate(page: 1)
-  #  first_page_of_users.each do |user|
-  #    assert_select 'a[href=?]', user_path(user), text: user.name
-  #    unless user == @admin
-  #      assert_select 'a[href=?]', user_path(user), text: 'delete'
-  #    end
-  #  end
-  #  assert_difference 'User.count', -1 do
-  #    delete user_path(@non_admin)
-  #  end
-  #end
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.where(admin: true).paginate(page: 1)
+    first_page_of_users.each do |user|
+      if user.admin?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
 
   test "index as non-admin" do
     log_in_as(@non_admin)
