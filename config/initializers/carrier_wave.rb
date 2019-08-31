740bedc90fa6b653b912abc426323762958f308c
The-Noble-K <jarod.sneed@gmail.com>
2019-08-31 14:07:26 -0400

    fix typo in carrier wave production settings

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    confi.fog_credentials = {
+    config.fog_credentials = {
       # Configuration for Amazon S3
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'], 
